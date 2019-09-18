1f327efe098dc3757e7692947e85c35a92fceb73
leeyunbo <servers1@naver.com>
2019-09-18 23:14:44 +0900

    처음

---
+++ app/src/main/java/com/example/weatherclothselection/Presenter/MainContract.kt
@@ -0,0 +1,17 @@
+package com.example.weatherclothselection.Presenter
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+interface MainContract {
+
+    interface View {
+
+    }
+
+    interface Presenter {
+
+    }
+
+
+}
