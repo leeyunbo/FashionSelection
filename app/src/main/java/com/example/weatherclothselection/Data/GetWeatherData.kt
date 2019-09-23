81fd8dc26e5fcc73d57e4c32bfd0e5e21d9d0792
leeyunbo <servers1@naver.com>
2019-09-23 23:30:35 +0900

    파싱 진행중

--- app/src/main/java/com/example/weatherclothselection/Data/GetWeatherData.kt
+++ app/src/main/java/com/example/weatherclothselection/Data/GetWeatherData.kt
@@ -5,15 +5,19 @@ import android.os.DropBoxManager
 import android.util.Xml
 import com.example.weatherclothselection.MainActivity
 import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserException
+import java.io.IOException
 import java.io.InputStream
 import java.net.URL
 import java.security.KeyStore
 
+private val ns: String? = null
+
 //"http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=4111757000"
 class GetWeatherData {
 
-    fun GetWeatherData() : List<*> {
-        lateinit var inputStream : InputStream
+    @Throws(XmlPullParserException::class, IOException::class)
+    fun GetWeatherData(inputStream : InputStream) : List<*> {
         inputStream.use { inputStream ->
             val parser: XmlPullParser = Xml.newPullParser()
             parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false)
@@ -24,10 +28,19 @@ class GetWeatherData {
 
     }
 
-    fun readFeed(parser : XmlPullParser) : List<KeyStore.Entry> {
+    @Throws(XmlPullParserException::class, IOException::class)
+    private fun readFeed(parser : XmlPullParser) : List<KeyStore.Entry> {
         val entries = mutableListOf<KeyStore.Entry>()
 
         parser.require(XmlPullParser.START_TAG,ns,"feed")
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if(parser.eventType != XmlPullParser.START_TAG) {
+                continue
+            }
+            if (parser.name == "channel") {
+                entries.add()
+            }
+        }
 
     }
 
